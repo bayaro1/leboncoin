919ffb7a868b61dd63ff16a694550003e82ba545
Vico <vico@lebongroin.fr>
2022-11-09 20:38:00 +0100

    preparation

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'home_index')]
+    public function index():Response
+    {
+        return $this->render('home/index.html');
+    }
+}
